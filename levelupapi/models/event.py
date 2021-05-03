5c4893ac6ad0586ca49f879228924ca783cc7eca
Caleb James <calebsjames@gmail.com>
2021-05-03 15:26:15 -0500

    feat: added model structure for database

---
+++ levelupapi/models/event.py
@@ -0,0 +1,16 @@
+from levelupapi.models.games import Game
+from levelupapi.models.gamer import Gamer
+from django.db import models
+
+
+class Event(models.Model):
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    host = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    description = models.CharField(max_length=200)
+
+
+
