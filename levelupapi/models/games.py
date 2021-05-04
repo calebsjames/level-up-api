e0b60a03685df26c653c34dbd6eb3513e39b0cf0
Caleb James <calebsjames@gmail.com>
2021-05-03 19:58:25 -0500

    first migration

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -1,10 +1,9 @@
-from levelupapi.models import GameType
 from django.db import models
 
 
 class Game(models.Model):
 
-    game_type = models.ForeignKey(GameType, on_delete=models.CASCADE)
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     player_limit = models.IntegerField()
     difficulty = models.IntegerField()
