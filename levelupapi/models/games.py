cfbe79d76ee230b22143044d254f63911886e92f
Caleb James <calebsjames@gmail.com>
2021-05-05 10:29:12 -0500

    feat: view.py, urls.py and models updated

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class Game(models.Model):
 
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
-    player_limit = models.IntegerField()
-    difficulty = models.IntegerField()
+    maker = models.CharField(max_length=30)
+    gamer = models.IntegerField()
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    number_of_players = models.IntegerField()
+    skill_level = models.IntegerField()
+
+    
    
