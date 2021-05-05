f267a2ebacdb6b4a7e5f176fae64a4c17df4cb19
Caleb James <calebsjames@gmail.com>
2021-05-05 11:13:11 -0500

    fix: gametype in models

--- levelupapi/models/games.py
+++ levelupapi/models/games.py
@@ -6,7 +6,7 @@ class Game(models.Model):
     title = models.CharField(max_length=30)
     maker = models.CharField(max_length=30)
     gamer = models.IntegerField()
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
+    gametype = models.ForeignKey("GameType", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
     skill_level = models.IntegerField()
 
