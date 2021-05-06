9251a262feac7d040ade69554a7bdc54caeeb019
Caleb James <calebsjames@gmail.com>
2021-05-05 22:33:22 -0500

    feat: user created events displayed at /events

--- levelupapi/migrations/0001_initial.py
+++ levelupapi/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Event',
+            name='Events',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=30)),
